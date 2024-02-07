8085bfb72f493aa9057e11a47751e4e44e16a20a
Cameron Gallichan <camerongallichan@gmail.com>
2024-02-07 02:23:30 +0000

    revert to redux createStore

--- src/store.js
+++ src/store.js
@@ -1,14 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
 import { thunk } from 'redux-thunk';
 import rootReducer from './reducers';
 
+const initialState = {};
+
 const middleware = [thunk];
 
-const preloadedState = {};
-const store = configureStore({
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(middleware),
-    reducer: rootReducer(),
-    preloadedState
-});
+//TODO: Use configureStore from @reduxjs/toolkit
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 export default store;
