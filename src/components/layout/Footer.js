6e5032413f9927aa6bcca551dc8d52b27524b889
Cameron Gallichan <camerongallichan@gmail.com>
2024-02-11 17:23:40 +0000

    simplified footer styling

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,13 +3,11 @@ import React from "react";
 const Footer = () => {
   return (
     <footer style={{height: 'auto', width: '100%', backgroundColor: '#E5E5E5'}}>
-        <div style={{margin: 'auto', maxWidth: '500px', textAlign: 'center', padding: '3rem 0rem'}}>
-            <div style={{fontWeight: 'bold'}}>Connect to us: </div>
-            <ul>
-                <li><i className='fa-brands fa-facebook'></i> Facebook</li>
-                <li><i className='fa-brands fa-x-twitter'></i> X/Twitter</li>
-                <li><i className='fa-brands fa-instagram'></i> Instagram</li>
-            </ul>
+        <div style={{margin: 'auto', maxWidth: '300px', textAlign: 'center', padding: '2rem 0rem'}}>
+            <p style={{fontWeight: 'bold'}}>Connect to us</p>
+            <p className='fa-brands fa-facebook' style={{padding: '0rem 0.5rem 0rem 0.5rem'}}/>
+            <p className='fa-brands fa-x-twitter' style={{padding: '0rem 0.5rem 0rem 1rem'}}/>
+            <p className='fa-brands fa-instagram' style={{padding: '0rem 0.5rem 0rem 1rem'}}/>
         </div>
         <div style={{textAlign: 'center', padding: '1rem 0rem', backgroundColor: '#343a40'}}>
             <p style={{color: 'white'}}>
