3173a69d7be5f5b4d022e237e8a9fc89242b8c1a
Cameron Gallichan <camerongallichan@gmail.com>
2024-02-12 17:43:25 +0000

    migrate client from react-scripts to vite

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(() => {
+  return {
+    build: {
+      outDir: 'build',
+    },
+    plugins: [react()],
+    server: {
+        port: 3000,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:5000',
+                changeOrigin: true,
+                secure: false,      
+                ws: true,
+            }
+        }
+    },
+    resolve: {
+        mainFields: []
+    }
+  };
+});
