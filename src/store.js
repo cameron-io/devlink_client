e720dd5c8d98236005f042cd934638672773933b
Cameron Gallichan <camerongallichan@gmail.com>
2024-06-28 14:02:48 +0100

    use @reduxjs/toolkit for configureStore

--- src/store.js
+++ src/store.js
@@ -1,17 +1,10 @@
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from '@redux-devtools/extension';
 import { thunk } from 'redux-thunk';
 import rootReducer from './reducers';
+import { configureStore } from '@reduxjs/toolkit';
 
-const initialState = {};
-
-const middleware = [thunk];
-
-//TODO: Use configureStore from @reduxjs/toolkit
-const store = createStore(
-  rootReducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
+const store = configureStore({
+    reducer: rootReducer,
+    middleware: () => [thunk],
+});
 
 export default store;
