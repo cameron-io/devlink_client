25611df2ca320bb1d7e594f40794d35ac8a43fa1
Cameron Gallichan <camerongallichan@gmail.com>
2026-05-05 22:59:01 +0100

    removed prepended https from gravatar url

--- src/components/profile/ProfileAvatar.tsx
+++ src/components/profile/ProfileAvatar.tsx
@@ -11,7 +11,7 @@ const Avatar = (props: AvatarProps) => (
     <Fragment>
         {props.avatar 
             && 
-            <img src={'https:' + props.avatar} alt="" className="border rounded-3" style={{height: props.size}} /> 
+            <img src={props.avatar} alt="" className="border rounded-3" style={{height: props.size}} /> 
             || 
             <div className="d-flex justify-content-center bg-black p-5 rounded" style={{height: props.size, width: props.size}} >
                 <FontAwesomeIcon icon={faUser} className='fa-6x'/>
